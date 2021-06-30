c8a366a05f5ab428bd3dbcf47bf57c05b8b1ef06
lls <lls.homeoffice@gmail.com>
2021-06-30 18:32:12 -0300

    Add files remotely

---
+++ bin/update_conf.sh
@@ -0,0 +1,32 @@
+JUNHO(2021){
+(28/06/2021){
+(OK) Criar novo Cloud para Funchal {
+ (ok) Criar novo Email no Google
+ (ok) Criar nova Conta na Amazon Web Service
+ (ok) Configurar Instância EC2
+ (ok) Editar Regras de Entrada
+ (ok) Adicionar Zona no Registro.br
+ (ok) GoogleDrive: user.doc
+ }
+}
+(29/06/2021){
+(OK) Configurar Usuário pelo GitHub {
+ (ok) user_conf.sh
+ }
+}
+(30/06/2021){
+(OK) Configurar GitHub {
+ (ok) GoogleDrive: token.doc
+ (ok) git_conf.sh
+ }
+}
+}
+
+JULHO(2021){
+(01/07/2021){
+(  ) Configurar Ubuntu pelo GitHub {
+ (  ) ubuntu_conf.sh
+ }
+}
+
+}
