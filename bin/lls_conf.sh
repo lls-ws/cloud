1c2f224dac18947996b5ec528afb92a5bd830b9f
lls <lls.homeoffice@gmail.com>
2021-07-03 17:37:14 -0300

    Add files remotely

--- bin/lls_conf.sh
+++ bin/lls_conf.sh
@@ -14,22 +14,44 @@ lls_create()
 	echo "Stopping tomcat..."
 	service tomcat stop
 	
-	tar -cvzf ${ARQ_LLS} ${DIR_LLS}
+	tar -cvzf ${ARQ_LLS} -C ${DIR_TOMCAT} ${DIR_LLS}
+	
+	tar -tf ${ARQ_LLS}
 	
 	du -hsc ${ARQ_LLS}
 	
 }
 
+lls_install()
+{
+	
+	echo "Stopping tomcat..."
+	service tomcat stop
+	
+	tar -xvzf ${ARQ_LLS} -C ${DIR_TOMCAT}
+	
+	chmow -R tomcat.tomcat ${DIR_TOMCAT}/${DIR_LLS}
+	
+	la -al ${DIR_TOMCAT}/${DIR_LLS}
+	
+	du -hsc ${DIR_TOMCAT}/${DIR_LLS}
+	
+}
+
 HOSTNAME=`hostname`
-ARQ_LLS="lls-${HOSTNAME}.tar.gz"
-DIR_LLS="/usr/share/tomcat/webapps/lls"
+DIR_LLS="lls"
+ARQ_LLS="${DIR_LLS}-${HOSTNAME}.tar.gz"
+DIR_TOMCAT="/usr/share/tomcat/webapps"
 
 case "$1" in
 	create)
 		lls_create
 		;;
+	install)
+		lls_install
+		;;
 	*)
-		echo "Use: $0 {create}"
+		echo "Use: $0 {create|install}"
 		exit 1
 		;;
 esac
