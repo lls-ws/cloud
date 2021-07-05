dc48ad7ff0eb749eb42c138661b9d2ccec8a0264
lls <lls.homeoffice@gmail.com>
2021-07-05 01:43:08 -0300

    Add files remotely

--- bin/lls_backup.sh
+++ bin/lls_backup.sh
@@ -60,7 +60,7 @@ backup_send()
 	
 	echo -e "to: ${DESTINATARIO}\nsubject: Backup LLS-WS\n" |
 	
-	(cat - && uuencode ${FILE_ZIP} ${FILE_ZIP}) |
+	(cat - && uuencode ${FILE_ZIP} "lls_backup.zip") |
 	
 	/usr/sbin/ssmtp ${DESTINATARIO}
 	
