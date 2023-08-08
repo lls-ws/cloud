e7af61099250996bd717d6e6268e108dc468989d
Leandro Luiz <69089565+lls-ws@users.noreply.github.com>
2023-08-08 12:11:20 -0300

    Update update_conf.sh

--- bin/update_conf.sh
+++ bin/update_conf.sh
@@ -96,9 +96,9 @@ AGOSTO(2023){
 (08/08/2023){
 
 () Configurar LLS-WS {
- () lls_conf.sh local <HOSTNAME>
- () lls_conf.sh create <HOSTNAME>
- () lls_conf.sh install <HOSTNAME>
+ () lls_conf.sh local <HOSTNAME_OLD>
+ () lls_conf.sh create <HOSTNAME_OLD>
+ () lls_conf.sh install <HOSTNAME_OLD>
  () lls_conf.sh server
  () lls_conf.sh update
  }
