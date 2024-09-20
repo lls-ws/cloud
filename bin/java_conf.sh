c5885e5bf4cbed4d4ac6a1052b02b82ab47cf584
Leandro Luiz <69089565+lls-ws@users.noreply.github.com>
2024-09-20 16:20:52 -0300

    Update java_conf.sh

--- bin/java_conf.sh
+++ bin/java_conf.sh
@@ -24,9 +24,10 @@ java_path()
 {	
 	
 	DIR_ENVIRONMENT="/etc/environment"
-	
+	JAVA_HOME="/usr/lib/jvm/java-${JAVA_VERSION}-openjdk-amd64"
+ 
 	echo "Set the Java home path..."
-	echo 'JAVA_HOME="/usr/lib/jvm/java-"${JAVA_VERSION}"-openjdk-amd64"' >> ${DIR_ENVIRONMENT}
+	echo 'JAVA_HOME="${JAVA_HOME}"' >> ${DIR_ENVIRONMENT}
 	
 	echo "Now reload this file to apply the changes to your current session:"
 	source ${DIR_ENVIRONMENT}
